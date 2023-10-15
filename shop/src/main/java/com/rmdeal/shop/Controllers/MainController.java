9f924eb02377f5e9375369235c01b1afefc86ded
rmchanski <114597907+rmchanski@users.noreply.github.com>
2023-10-16 00:01:41 +0300

    Add files via upload

---
+++ shop/src/main/java/com/rmdeal/shop/Controllers/MainController.java
@@ -0,0 +1,30 @@
+package com.rmdeal.shop.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "RmDeal");
+        return "home";
+    }
+    @GetMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("title", "Каталог");
+        return "list";
+    }
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "О нас");
+        return "about";
+    }
+    @GetMapping("/help")
+    public String help(Model model) {
+        model.addAttribute("title", "Помощь");
+        return "help";
+    }
+}
